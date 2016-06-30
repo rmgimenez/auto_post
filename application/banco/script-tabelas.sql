75e3a5681531614a1701a66eac97b8b1381a907a
Ricardo Gimenez <rmgimenez@gmail.com>
2016-06-30 13:51:49 -0300

    alteração na estrutura do banco

--- application/banco/script-tabelas.sql
+++ application/banco/script-tabelas.sql
@@ -39,3 +39,17 @@ create table postagens(
     data_post timestamp,
     destino_postado integer
 );
+
+----
+-- Table structure for senders
+----
+CREATE TABLE 'senders' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'email' TEXT, 'smtp_secure' TEXT, 'username' TEXT, 'password' TEXT, 'form' TEXT, 'host' TEXT, 'port' INTEGER, 'is_smtp' INTEGER, 'smtp_auth' INTEGER, 'ativo' INTEGER);
+INSERT INTO "senders" ("id","email","smtp_secure","username","password","form","host","port","is_smtp","smtp_auth","ativo") VALUES ('1','rmgimenez1@hotmail.com','tls','rmgimenez1@hotmail.com','moura85@','rmgimenez1@hotmail.com','smtp.live.com','587','1','1','1');
+
+----
+-- Table structure for origens_imgur
+----
+CREATE TABLE 'origens_imgur' ('id' INTEGER PRIMARY KEY NOT NULL, 'nome' TEXT, 'nome_reddit' TEXT, 'tags' TEXT, 'grupo' TEXT, 'ativo' INTEGER DEFAULT 1 );
+INSERT INTO "origens_imgur" ("id","nome","nome_reddit","tags","grupo","ativo") VALUES ('1','r/cosplay','cosplay','#cosplay #costumes','cosplay','1');
+
+COMMIT;
