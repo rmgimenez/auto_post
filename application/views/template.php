77c295df5f2dcb83940f5267b25d77cfc81229e9
Ricardo Gimenez <rmgimenez@gmail.com>
2016-07-04 16:58:00 -0300

    coloquei o bootstrap como layout

--- application/views/template.php
+++ application/views/template.php
@@ -1,40 +1,28 @@
-<html>
-<head>
-<?php
-    if(!(isset($pagina_titulo)))
-    {
-        $pagina_titulo = 'Auto Post';
-    }
-?>
-    <title><?php echo $pagina_titulo; ?></title>
-    
-    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css"/>
-    
-<style>
-.conteudo{
-    margin: auto;
-    width: 75%;
-    border: 3px solid black;
-    padding: 10px;
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
 
-.borda{
-    border: 3px solid black;
-}
-</style>
-</head>
-<body>
-<div class="conteudo">
-    <div class="pure-g">
-        <div class="pure-u-1-5"><p>Menu lateral</p></div>
-        <div class="pure-u-4-5">
-            <h2>Corpo do site</h2>
-            <p>Uma linha</p>
-        </div>
-    </div>
-</div>
-<script>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
 
-</script>
-</body>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
